958fde8e59349a271c932be916e31715d8f24739
Blickle Florian <f.blickle@sandata.de>
2024-01-17 13:26:22 +0100

    Second Commit

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
-from typing import List
 import pandas as pd
 import yaml
 from pydantic import BaseModel
-import json
 
 api_titel = "SanDyy API Container"
 api_version = "1.0.2"
@@ -47,8 +45,8 @@ class TicketOrder (BaseModel):
 app = FastAPI(openapi_version="3.0.1")
 
 
-@app.get("/search/{query}")
-def search(query: str):
+@app.get("/search_in_asset_list/{query}")
+def search_in_asset_list(query: str):
     # Read the data from an Excel file
     df = pd.read_excel("asset-list.xlsx")
     # Create an empty list to store the JSON objects
@@ -62,15 +60,13 @@ def search(query: str):
             # Add the JSON object to the json_results list
             json_results.append(device_item)
     # Return the json_results list
-    return json_results
+    return {"Results": json_results}
 
 
 @app.get("/get_device_by_id/{id}")
 def get_device_by_id(id: str):
     # Read the data from an Excel file
     df = pd.read_excel("asset-list.xlsx")
-    # Create an empty list to store the JSON objects
-    json_results = []
     # Iterate over each row in the DataFrame
     for index, row in df.iterrows():
         # Check if the userid is present in the MitarbeiterID column
@@ -80,27 +76,21 @@ def get_device_by_id(id: str):
                                      manufacturercode=str(row["Herstellercode"]),
                                      manufacturer_article_id=str(row["Herstellerartikelnr."]),
                                      searchstring=row["Suchbegriff"])
-            # Add the JSON object to the json_results list
-            json_results.append(device_item)
-    # Return the json_results list
-    return json_results
+            return {"DeviceItem": device_item}
+
 
 
 @app.get("/get_user_by_userid/{userid}")
 def get_user_by_userid(userid: int):
     # Read the data from an Excel file
     df = pd.read_excel("hierachy.xlsx")
-    # Create an empty list to store the JSON objects
-    json_results = []
     # Iterate over each row in the DataFrame
     for index, row in df.iterrows():
         # Check if the userid is present in the MitarbeiterID column
         if row["MitarbeiterID"] == userid:
             user = User(userid=userid, username=row["MitarbeiterName"], usermail=row["Mail"], superior_id=row["VorgesetzterID"])
             # Add the JSON object to the json_results list
-            json_results.append(user)
-    # Return the json_results list
-    return json_results
+        return {"User": user}
 
 
 @app.get("/get_devices_by_userid/{userid}")
@@ -113,8 +103,6 @@ def get_devices_by_userid(userid: int):
     for index, row in df.iterrows():
         # Check if the userid is present in the MitarbeiterID column
         if row["MitarbeiterID"] == userid:
-            #user = User(userid=userid, username=row["MitarbeiterName"], superior_id=)
-            #json_results.append(user)
             # If the userid is present, create a DeviceItem object and fill the attributes with the values from the Excel file
             device_item = DeviceItem(id=str(row["Nr."]), description=row["Beschreibung"], stock=row["Menge"],
                                      manufacturercode=str(row["Herstellercode"]),
@@ -123,7 +111,7 @@ def get_devices_by_userid(userid: int):
             # Add the JSON object to the json_results list
             json_results.append(device_item)
     # Return the json_results list
-    return json_results
+    return {"DeviceItem": json_results}
 
 
 @app.post("/add_ticket_it_support")
@@ -146,7 +134,7 @@ def add_ticket_it_support(ticket_it_support: TicketItSupport):
     df.to_excel("ticket-list-it-support.xlsx", index=False)
     # Methode zum Schreiben einer Teams Nachricht
     # Return a success message
-    return ticket_it_support
+    return {"TicketSupport:": ticket_it_support}
 
 
 @app.post("/add_ticket_orders")
@@ -169,7 +157,7 @@ def add_ticket_it_support(ticket_order: TicketOrder):
     # Methode zum Schreiben einer Teams-Nachricht
 
     # Return a success message
-    return ticket_order
+    return {"TicketBestellung": ticket_order}
 
 
 @app.post("/add_item")
